c48b7ccf6428da107f927eeda4eab2954762ab1a
sanko <sang.koh@hotmail.com>
2024-12-11 22:49:08 +1100

    add sample tui

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+
+use ratatui::{
+    crossterm::event::{self, KeyCode, KeyEventKind},
+    style::Stylize,
+    widgets::Paragraph,
+    DefaultTerminal,
+};
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    terminal.clear()?;
+    let app_result = run(terminal);
+    ratatui::restore();
+    app_result
+}
+
+fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
+    loop {
+        terminal.draw(|frame| {
+            let dragons = Paragraph::new("here be dragons!")
+                .white()
+                .on_red();
+            frame.render_widget(dragons, frame.area());
+        })?;
+
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(());
+            }
+        }
+    }
+}
