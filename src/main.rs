f2bd3785ce8a2b715632516a6d5627bfc5e0dc22
sanko <sang.koh@hotmail.com>
2024-12-15 19:02:53 +1100

    select and highlight items

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use ratatui::{
     buffer::Buffer,
     crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
     layout::{Layout, Rect, Constraint, Direction},
+    style::Stylize,
     symbols::border,
     text::Line,
     widgets::{Block, Paragraph, Widget},
@@ -72,6 +73,7 @@ fn main() -> io::Result<()> {
 
 #[derive(Default)]
 pub struct Todo {
+    current: (bool, usize),
     list: Vec<Stuff>,
     exit: bool,
 }
@@ -102,6 +104,20 @@ impl Todo {
     fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
             KeyCode::Char('q') => self.exit(),
+            KeyCode::Char('j') => {
+                self.current.1 += 1;
+            },
+            KeyCode::Char('k') => {
+                if self.current.1 > 0 {
+                    self.current.1 -= 1;
+                }
+            },
+            KeyCode::Char('h') => {
+                self.current.0 = !self.current.0;
+            },
+            KeyCode::Char('l') => {
+                self.current.0 = !self.current.0;
+            },
             _ => {},
         };
     }
@@ -115,9 +131,14 @@ impl Widget for &Todo {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let todo: Vec<_> = self.list.iter()
             .filter(|stuff| !stuff.done)
-            .map(|stuff| {
+            .enumerate()
+            .map(|(i, stuff)| {
                 let string = format!(" {} - {} ", stuff.id.to_string(), stuff.text.clone());
-                Line::from(string)
+                if !self.current.0 && self.current.1 == i {
+                    Line::from(string.white().on_red())
+                } else {
+                    Line::from(string)
+                }
             })
             .collect();
         let todo_title = Line::from(" todo ");
@@ -127,9 +148,14 @@ impl Widget for &Todo {
 
         let done: Vec<_> = self.list.iter()
             .filter(|stuff| stuff.done)
-            .map(|stuff| {
+            .enumerate()
+            .map(|(i, stuff)| {
                 let string = format!(" {} - {} ", stuff.id.to_string(), stuff.text.clone());
-                Line::from(string)
+                if self.current.0 && self.current.1 == i {
+                    Line::from(string.white().on_red())
+                } else {
+                    Line::from(string)
+                }
             })
             .collect();
         let done_title = Line::from(" done ");
